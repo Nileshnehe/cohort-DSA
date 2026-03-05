c4a8e88058deafdd55a15c43b854b57734ab8b02
Nileshnhe <nehenilesh111@gmail.com>
2026-03-05 09:35:07 +0530

    Renamed app.js to 01-print-1-to-n.js

--- Loop-pt1/01-print-1-to-n.js
+++ Loop-pt1/01-print-1-to-n.js
@@ -1,3 +1,8 @@
+// Problem: Print numbers from 1 to N
+// Input: N
+// Output: 1 2 3 ... N
+// Time Complexity: O(n)
+
 let prompt = require('prompt-sync')();
 let n = Number(prompt("Enter a number"))
 
